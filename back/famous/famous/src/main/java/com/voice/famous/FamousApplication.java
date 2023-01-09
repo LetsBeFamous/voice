7bf6104f8e6e9ee43ac0f1dcd229555fe0852320
heesun <heesunmelody@gmail.com>
2023-01-09 20:46:10 +0900

    initial commit

---
+++ back/famous/famous/src/main/java/com/voice/famous/FamousApplication.java
@@ -0,0 +1,13 @@
+package com.voice.famous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamousApplication.class, args);
+	}
+
+}
---
+++ back/famous/famous/src/test/java/com/voice/famous/FamousApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voice.famous;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamousApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
